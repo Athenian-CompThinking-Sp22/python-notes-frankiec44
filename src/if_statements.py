24335dfc37adafb817992610de9bda6450674621
francesclulow <25fclulow@athenian.org>
2022-04-12 14:02:37 -0700

    april 7, 2022 code

--- src/if_statements.py
+++ src/if_statements.py
@@ -1,6 +1,17 @@
 #############################
+
 # Collaborators: (enter people or resources who/that helped you)
 # If none, write none
 #
 #
 #############################
+play= input("do you want to play a game? (Type yes or no) ")
+
+if play=="no" or play=="No" or play=="NO":
+    print("ok,bye!")
+
+elif play== "Yes" or play== "yes" or play== "YES":
+    print("great, let's go!")
+
+else:
+    print("follow directions ")
--- src/variables.py
+++ src/variables.py
@@ -4,3 +4,19 @@
 #
 #
 #############################
+name =input("please enter your name")
+print(f"hi {name}")
+print(type(name))
+num1=input("please enter a number to multiply by 2:")
+num1=int(num1)
+answer= num1*2
+print(answer)
+num2=input("enter a random number to divide by 9")
+num2=int(num2)
+answer= num2/9
+print(answer)
+
+aa= input("how was ur day?")
+print(type)(aa)
+print(f"no one cares {name}")
+print(type(name))
